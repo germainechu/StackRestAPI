ac47215fce022715efdbc9c817a2248cc0509aee
gchu <g0xchu@gmail.com>
2020-05-11 14:06:31 -0700

    missing tests, but get, delete and put all working

--- src/main/java/com/example/stack/StackApplication.java
+++ src/main/java/com/example/stack/StackApplication.java
@@ -3,56 +3,62 @@ package com.example.stack;
 import java.util.concurrent.atomic.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootApplication
 @RestController
 public class StackApplication {
 
 	AtomicReferenceArray<Node<Object>> top = new AtomicReferenceArray<Node<Object>>(10000);
+	private int size = 0;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StackApplication.class, args);
 	}
 
-	@RequestMapping(value = "/stack", method = RequestMethod.GET)
+	@GetMapping(value = "/stack/size")
 	public int stack() {
-		return top.length();
+		return size;
+		
 	}
 
-	@RequestMapping(value = "/stack/push/{item}", method = RequestMethod.PUT)
-	public @ResponseBody void push(@RequestBody Object item) {
+	@PutMapping(value = "/stack/{item}")
+	public @ResponseBody void push(@PathVariable Object item) {
 		Node<Object> newHead = new Node<Object>(item);
 		Node<Object> oldHead;
+		size++;
 		do {
-			oldHead = top.get(top.length() - 1);
+			oldHead = top.get(size - 1);
 			newHead.next = oldHead;
 
-		} while (!top.compareAndSet(0, oldHead, newHead));
+		} while (!top.compareAndSet(size - 1, oldHead, newHead));
 
 	}
 
 	@RequestMapping(value = "/stack/pop", method = RequestMethod.DELETE)
 	public @ResponseBody Object pop() {
-		if (top.length() == 0) {
-			try {
-				throw new Exception("Error Code: 100");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		if (size == 0) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
 		}
 		Node<Object> oldHead;
         Node<Object> newHead;
         do {
-            oldHead = top.get(top.length()-1);
+            oldHead = top.get(size - 1);
             if (oldHead == null)
                 return null;
-            newHead = oldHead.next;
-        } while (!top.compareAndSet(0,oldHead, newHead));
+			newHead = oldHead.next;
+			
+		} while (!top.compareAndSet(size - 1,oldHead, newHead));
+		size--;
         return oldHead.item;
 	}
 
