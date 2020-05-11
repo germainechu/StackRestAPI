b8a75e6edf211fd7da6399b4470c1ce817754302
gchu <g0xchu@gmail.com>
2020-05-10 23:35:32 -0700

    missing tests

---
+++ src/main/java/com/example/stack/StackApplication.java
@@ -0,0 +1,71 @@
+package com.example.stack;
+
+import java.util.concurrent.atomic.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StackApplication {
+
+	AtomicReferenceArray<Node<Object>> top = new AtomicReferenceArray<Node<Object>>(100);
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackApplication.class, args);
+	}
+
+	@RequestMapping(value = "/stack", method = RequestMethod.GET)
+	public int stack() {
+		return top.length();
+	}
+
+	@RequestMapping(value = "/stack/push/{item}", method = RequestMethod.PUT)
+	public @ResponseBody void push(@RequestBody Object item) {
+		Node<Object> newHead = new Node<Object>(item);
+		Node<Object> oldHead;
+		do {
+			oldHead = top.get(top.length() - 1);
+			newHead.next = oldHead;
+
+		} while (!top.compareAndSet(0, oldHead, newHead));
+
+	}
+
+	@RequestMapping(value = "/stack/pop", method = RequestMethod.DELETE)
+	public @ResponseBody Object pop() {
+		if (top.length() == 0) {
+			try {
+				throw new Exception("Error Code: 100");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		Node<Object> oldHead;
+        Node<Object> newHead;
+        do {
+            oldHead = top.get(top.length()-1);
+            if (oldHead == null)
+                return null;
+            newHead = oldHead.next;
+        } while (!top.compareAndSet(0,oldHead, newHead));
+        return oldHead.item;
+	}
+
+	private static class Node<E> {
+		public final Object item;
+		public Node<Object> next;
+
+		public Node(E item) {
+			this.item = item;
+		}
+
+
+	}
+
+
+}
---
+++ src/test/java/com/example/stack/StackApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+	@Test
+	void testGET() {
+		
+	}
+
+}
