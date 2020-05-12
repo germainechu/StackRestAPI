0ac0b1914a0582936a4f4062233cd473de526b23
gchu <g0xchu@gmail.com>
2020-05-12 00:20:09 -0700

    added test skeletons

--- src/test/java/com/example/stack/StackApplicationTests.java
+++ src/test/java/com/example/stack/StackApplicationTests.java
@@ -1,19 +1,52 @@
 package com.example.stack;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class StackApplicationTests {
+
+
+
+@SpringBootTest 
+public class StackApplicationTests {
+	
+	WebTestClient testClient;
+	
+	@BeforeEach
+	void contextLoads() {
+		testClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
+	}
 
 	@Test
-	void contextLoads() {
+	void testGetStackSizeWhenEmpty() {
+		System.out.println(testClient.get().uri("/stack/size"));
 
 	}
 
 	@Test
-	void testGET() {
-		
+	void testGetStackSizeWhenNotEmpty() {
+
+	}
+
+	@Test
+	void testPutIntoStack() {
+
+	}
+
+	@Test
+	void testMultiplePutOneDelete() {
+
+	}
+
+	@Test
+	void testMultiplePutMultipleDelete() {
+
+	}
+
+	@Test
+	void testDeleteWhenEmpty_ExceptionHandling() {
+
 	}
 
 }
